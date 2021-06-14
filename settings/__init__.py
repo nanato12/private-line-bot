ded39628f4f3aadb1685c6bc1b308cbf9ad63a65
nanato12 <admin@nanato12.info>
2021-06-14 23:06:33 +0900

    Add settings

---
+++ settings/__init__.py
@@ -0,0 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+EMAIL = os.environ.get("LINE_PRIVATE_BOT_EMAIL")
+PASSWORD = os.environ.get("LINE_PRIVATE_BOT_PASSWORD")
