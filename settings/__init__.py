88d028bfb896d12ed72166cb75242d8bcf39f07f
nanato12 <admin@nanato12.info>
2021-06-15 23:56:54 +0900

    Update settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 EMAIL = os.environ.get("LINE_PRIVATE_BOT_EMAIL")
 PASSWORD = os.environ.get("LINE_PRIVATE_BOT_PASSWORD")
 AUTH_TOKEN = os.environ.get("LINE_PRIVATE_BOT_AUTH_TOKEN")
+
+ADMIN_MIDS = ["u47689615351ce76d35a123c2f415faa0"]
